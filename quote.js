9272691f3e7feab93ccb329900139831735dedb4
i319755 <dylan.peters@student.fontys.nl>
2016-06-07 20:32:59 +0200

    Opgeschoond

--- quote.js
+++ quote.js
@@ -4,44 +4,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	//get the quote div
 	var x = document.getElementById("quote");
 	
-	//console.log(x);
-	//console.log(x.children[1].innerHTML);
-	
-	//get the quote lines to add a element
-	//old = document.getElementById('quote').innerHTML;
-	
-	//document.getElementById('quote').innerHTML = old + '<p class="singleQuote">' + x.children[1].innerHTML + '</p>' ;
-	
-	//document.getElementById("demo").innerHTML = x;  
-	
 	//funtion for creation of the random quote
 	function randomQuote(){
 		 
 		totalQuoteLines = x.getElementsByTagName("p").length;
-		console.log(totalQuoteLines);
-
 		random = Math.floor((Math.random() * totalQuoteLines));
-		console.log(random);
-		//document.getElementById("demo").innerHTML = random;
-		
 		randomQuote = x.children[random].innerHTML;
-		console.log(randomQuote);
 	}
 	randomQuote();
 	
-	//console.log(random);
-	//console.log(randomQuote);
-	
 	//function for appending the code to the quote div
-	function appendQuote(){
-		
+	function appendQuote(){	
 		document.getElementById("quote").innerHTML = '<p class="singleQuote">' + randomQuote + '</p>';
-		
-		console.log(document.querySelector(".singleQuote"));
 	}
 	appendQuote();	
 });
-
 
 var quoteFramework = quoteFramework || (function(){
     var properties = {}; 
@@ -58,7 +35,6 @@ var quoteFramework = quoteFramework || (function(){
 			});
        }
     };
- 
 }());
 
 
