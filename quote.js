1a5141126ccb1d53399225d835c1bc345e4be1b7
i319755 <dylan.peters@student.fontys.nl>
2016-06-07 13:10:06 +0200

    3 verschillende manieren
    random cijfer gebaseerd op de aantal p tagss

--- quote.js
+++ quote.js
@@ -1,6 +1,9 @@
-function myFunction() {
+/*function myFunction() {
 		var quotes = [];
 		var x = document.getElementsByTagName("p");
+		
+		console.log(document.getElementById("quote"));
+		console.log(x);
 		
 		document.getElementById("demo").innerHTML = x;  
 		
@@ -11,5 +14,87 @@ function myFunction() {
 		console.log(quotes);
 	}
 }
+
+
+
+
+function myFunction() {
+		var quotes = [];
+		//var x = document.getElementsByTagName("p");
+		
+		var x = document.getElementById("quote");
+		console.log(x);
+		console.log(x.getElementsByTagName("p").length);
+		
+		document.getElementById("demo").innerHTML = x;  
+		
+		for(var i = 0; i < x.getElementsByTagName("p").length; i++)
+	{
+		quotes.push(x.getElementsByTagName("p")[i].innerHTML);
+		//alert(x[i].innerHTML);
+		console.log(quotes);
+	}
+}
+
+*/
+
+/*
+
+function myFunction() {	
+
+	//get the quote div
+	var x = document.getElementById("quote");
+	
+	//console.log(x);
+	//console.log(x.children[1].innerHTML);
+	
+	//get the quote lines
+	old = document.getElementById('quote').innerHTML;
+	
+	document.getElementById('quote').innerHTML = old + '<p class="singleQuote">' + x.children[1].innerHTML + '</p>' ;
+	
+	document.getElementById("demo").innerHTML = x;  
+	
+
+	function randomQuote(){
+		 
+		var = totalQuoteLines = x.getElementsByTagName("p").length);
+		console.log(totalQuoteLines);
+	
+	}
+	
+	randomQuote();
+	
+}
+*/
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	
+	//get the quote div
+	var x = document.getElementById("quote");
+	
+	//console.log(x);
+	//console.log(x.children[1].innerHTML);
+	
+	//get the quote lines
+	old = document.getElementById('quote').innerHTML;
+	
+	document.getElementById('quote').innerHTML = old + '<p class="singleQuote">' + x.children[1].innerHTML + '</p>' ;
+	
+	document.getElementById("demo").innerHTML = x;  
+	
+
+	function randomQuote(){
+		 
+		var totalQuoteLines = x.getElementsByTagName("p").length;
+		console.log(totalQuoteLines);
+
+		var random = Math.floor((Math.random() * totalQuoteLines));
+		document.getElementById("demo").innerHTML = random;
+	
+	}
+	randomQuote();
+});
+
 
 
